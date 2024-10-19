6ec94a3e5d210b33d4aed85578cf2ed0830078b0
logesh-works <logeshkumar.dev@gmail.com>
2024-10-20 00:58:55 +0530

    db:change

--- csc_app/settings.py
+++ csc_app/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = "csc_app.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-'''
+
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
@@ -113,7 +113,7 @@ DATABASES = {
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
